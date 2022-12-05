f979825d9ef2108ed36e7ffd402e9bce91acbdc8
elumalaifamil <119782297+elumalaifamil@users.noreply.github.com>
2022-12-05 11:56:35 +0100

    Update translation.py

--- translation.py
+++ translation.py
@@ -6,14 +6,14 @@ class Translation(object):
 Myself Lazy-MissPerfect !
 You can upload all https link including sony urls, voot, etc... & direct links \n\n Thank you @mRiderDM for this awesome bot
 
-/help for more details!"""
+"""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
     DOWNLOAD_START = "⚡️ **Downloading**..."
     UPLOAD_START = "⬇️ **Uploading**..."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @\n\n<b>Join : @LazyDeveloper</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @\n\n<b>Join : @all_is_well_friends</b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     SAVED_CUSTOM_THUMB_NAIL = "Custom thumbnail saved. This image will be used in both video & file ✅."
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully ✅."
