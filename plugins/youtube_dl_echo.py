31027972a4e4b16254eb9c52eba234a21c7c29b5
elumalaifamil <119782297+elumalaifamil@users.noreply.github.com>
2022-12-05 12:23:15 +0100

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -211,7 +211,7 @@ async def echo(bot, update):
                     callback_data=(cb_string_video).encode("UTF-8")
                 ),
                 InlineKeyboardButton(
-                    "file",
+                    "file" + format_ext,
                     callback_data=(cb_string_file).encode("UTF-8")
                 )
             ])
